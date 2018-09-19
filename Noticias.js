d9c1b71b36e1dc3f6c2f01e998ba5e944855ab26
João Victor Gomes Cabral <jv_cabral@outlook.com>
2018-09-19 00:30:43 -0300

    git commit all

---
+++ Noticias.js
@@ -0,0 +1,18 @@
+var http = require('http');
+
+http.createServer( function(req, res){
+
+    var categoria = req.url;
+
+    if(categoria == '/tecnologia'){
+        res.end("<html><body>Tecnologia</body></html>");
+    } else if(categoria == '/moda'){
+        res.end("<html><body>Moda</body></html>");
+    } else if(categoria == '/beleza'){
+        res.end("<html><body>Beleza</body></html>");
+    } else{
+        res.end("<html><body>Portal de noticias</body></html>");
+    }
+ 
+
+}).listen(3000);
