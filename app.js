18eb421dc2819271683e9b24a83ec0dded146a0a
João Victor Gomes Cabral <jv_cabral@outlook.com>
2018-09-19 00:30:58 -0300

    second commit

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 var express = require('express');
+var msg = require('./mod_teste');
 var app = express();
 
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.send("<html><body>Portal de noticias</body></html>")
+    res.render("home/index");
 });
 
-app.get('/tecnologia', function (req, res) {
-    res.render("secao/tecnologia");
+app.get('/ ', function (req, res) {
+    res.render("admin/form_add_noticia");
+});
+
+app.get('/formulario_inclusao_noticia', function (req, res) {
+    res.render("noticias/noticias");
 });
 
 app.listen(3000, function () {
-    console.log("Teste com express");
+    console.log(msg());
 });
